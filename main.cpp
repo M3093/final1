e85f828c7fe9bb52b075c797bd862a78188e4d2c
MariaBadilla <>
2021-05-18 15:53:54 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+//Programa de facturación// 
+
+#include <iostream>
+using namespace std;
+// Declaración de variables// 
+  int consumo;
+  char palabra [50]; 
+  int horas;
+  int watts;
+
+int main() 
+ 
+{
+   cout << " Nombre del dispositivo IOT: " <<endl;
+   cin>>palabra;
+   cout<<"Tipo: "<<endl;
+   cin>>palabra;
+   cout<<"Digite consumo: "<<endl;
+   cin>>consumo;
+   cout << "__________________________ " <<endl;
+
+  cout << "  Tiempo de dispositivo IOT: " <<endl;
+  cin>>horas;
+  cout << "__________________________ " <<endl;
+
+  watts=(horas*consumo);
+  cout << "  Consumo total de dispositivo IOT: " <<watts<<endl;
+    return 0;
+    }
